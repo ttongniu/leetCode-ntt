eed9e89f31da36fcc9bee961c0b50d0fc7304bac
ttongniu <ttongniu@163.com>
2019-06-01 18:12:28 +0800

    init

--- leetCode-ntt/src/com/company/Main.java
+++ leetCode-ntt/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Student student = new Student();
+        student.get().stream().findFirst().orElse(null);
     }
+
+
+}
+class  Student{
+    public Student(){
+
+    }
+    List<String>  list;
+    public List<String>  get(){
+        return  list;
+    }
+
+
+
 }
