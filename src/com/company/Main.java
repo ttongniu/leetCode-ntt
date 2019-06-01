6d1b59462096feb0b921792c528862befa1c3fe0
ttongniu <ttongniu@163.com>
2019-06-01 23:38:59 +0800

    提交

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.get().stream().findFirst().orElse(null);
+    }
+
+
+}
+class  Student{
+    public Student(){
+
+    }
+    List<String>  list;
+    public List<String>  get(){
+        return  list;
+    }
+
+
+
+}
